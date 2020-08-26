3e4e41a2b45465f3e8c75716ab11865cc89403b7
Jonny Townend <jonathan.townend@gmail.com>
2020-08-26 20:19:40 +0100

    Updates

---
+++ precache-manifest.9bc1c174cf6a5b3c48209d934501b6da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfbc9a210f1df839e75cf270ce05b4dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4a8da696a3e6fe6ad42",
+    "url": "/static/css/main.edc01b1d.chunk.css"
+  },
+  {
+    "revision": "d7992c58ec39bad80907",
+    "url": "/static/js/2.899f754c.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.899f754c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4a8da696a3e6fe6ad42",
+    "url": "/static/js/main.97e54e3d.chunk.js"
+  },
+  {
+    "revision": "fab5739b8e6d4b495c83",
+    "url": "/static/js/runtime-main.df853295.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
